df792bf37450d8b8c6a1e4fc14ddbe49b3eac62f
asgwey3 <156512224+asgwey3@users.noreply.github.com>
2026-01-02 02:28:30 +0800

    Update vless-server.sh

--- vless-server.sh
+++ vless-server.sh
@@ -13008,7 +13008,7 @@ main_menu() {
     init_db   # 初始化 JSON 数据库
     
     # 自动更新系统脚本 (确保 vless 命令始终是最新版本)
-    _auto_update_system_script
+    #_auto_update_system_script
     
     while true; do
         _header
